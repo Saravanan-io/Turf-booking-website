10a344c9e418fd8e53d8023dc471cf7bbe02f77f
J Saravanan <thisissaravanan84@gmail.com>
2025-09-18 17:51:21 +0530

    turf file added

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// Booking Form
+
     document.getElementById("bookingForm").addEventListener("submit", function(e) {
       e.preventDefault();
       alert("🎉 Your turf has been booked successfully!");
       this.reset();
     });
 
-    // Sticky header
     window.addEventListener("scroll", function(){
       const header = document.querySelector("header");
       header.classList.toggle("sticky", window.scrollY > 50);
     });
 
-    // Reveal animations on scroll
+  
     function revealOnScroll() {
       const form = document.querySelector("form");
       const galleryImages = document.querySelectorAll(".gallery-grid img");
